97ab38238d74b78099bf822f1f36e3951d7f9ac8
Marina Baucks <marina.baucks@kit.edu>
2025-12-12 17:23:23 +0100

    cleaned data folders

--- main.py
+++ main.py
@@ -3,7 +3,7 @@ from src.crawler import crawl_and_download, merge_images, process_images
 from src.utils import load_yaml_data
 
 # Load config
-config = load_yaml_data('config.yaml')
+config = load_yaml_data('crawl.yaml')
 
 # Crawl images
 if config['task'] == 'crawl':
